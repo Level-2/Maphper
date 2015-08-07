db84f810c9b13075a1b1b8f5e0ef395e3761f52b
Tom <tom@r.je>
2015-08-07 16:56:36 +0100

    Added sqlite test database to .gitignore

--- tests/SqliteDatabaseTest.php
+++ tests/SqliteDatabaseTest.php
@@ -9,12 +9,23 @@ class SqliteDatabaseTest extends MySqlDatabaseTest {
 	
 	public function __construct() {
 		parent::__construct();
-		$this->pdo = new PDO('sqlite:./test.db');
-		$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		
 		//prevent any Date errors
 		date_default_timezone_set('Europe/London');
 	}
 	
+
+	protected function setUp() {
+		parent::setUp ();
+		$this->pdo = new PDO('sqlite:./test.db');
+		$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+
+	protected function tearDown() {
+		$this->pdo = null;
+	}
+
+
 	//@Override
 	protected function tableExists($name) {
 		$result = $this->pdo->query('SELECT * FROM sqlite_master WHERE type="table" and name="'. $name.'"');
@@ -25,6 +36,23 @@ class SqliteDatabaseTest extends MySqlDatabaseTest {
 	protected function dropTable($name) {
 		$this->pdo->query('DROP TABLE IF EXISTS ' . $name);
 	}
-	
+
+	protected function getColumnType($table, $column) {
+		$result = $this->pdo->query('PRAGMA table_info(' . $table . ');')->fetchAll(\PDO::FETCH_OBJ);
+		foreach ($result as $row) {
+			if (strtolower($row->name) == $column) return $row->type;
+		}
+	}
+
+	//@Override
+	protected function hasIndex($table, $column) {
+		//$result = $this->pdo->query('indices ' . $table)->fetchAll(\PDO::FETCH_OBJ);
+		$result = $this->pdo->query('PRAGMA index_list(' . $table . ')')->fetchAll();
+		foreach ($result as $index) {
+			if (strtolower($index['name']) == strtolower($column)) return true;
+		}
+		return false;
+
+	}
 
 }
