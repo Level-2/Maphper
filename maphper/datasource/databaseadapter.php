f44b3781238f56af258e78febdb81e6d26b7a5b2
Tom <tom@r.je>
2015-08-08 14:06:10 +0100

    Updated databaseAdapter interface removing methods that are now in QueryBuilde

--- maphper/datasource/databaseadapter.php
+++ maphper/datasource/databaseadapter.php
@@ -1,14 +1,10 @@
 <?php 
 namespace Maphper\DataSource;
 interface DatabaseAdapter {
-	/*
+	public function query(\Maphper\DataSource\Database\Query $query);
 	public function quote($str);
-	public function delete($table, array $criteria, $args, $limit = null, $offset = null);
-	public function select($table, array $criteria, $args, $order = null, $limit = null, $offset = null);
-	public function aggregate($table, $function, $field, $where, $args, $group);
-	public function insert($table, array $primaryKey, $data);
 	public function alterDatabase($table, array $primaryKey, $data);
 	public function addIndex($table, array $fields);
 	public function lastInsertId();
-	*/
+	public function optimiseColumns($table);
 }
