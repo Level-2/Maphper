422645ffd187423487cab0059a9e01dc5d415b69
Tom <tom@r.je>
2015-08-08 11:30:51 +0100

    Added proper autoloader

--- tests/autoload.php
+++ tests/autoload.php
@@ -1,14 +1,6 @@
 <?php
 require_once 'tests/testclasses.php';
-require_once 'maphper/datasource.php';
-require_once 'maphper/maphper.php';
-require_once 'maphper/multipk.php';
-require_once 'maphper/relation.php';
-require_once 'maphper/relation/one.php';
-require_once 'maphper/relation/many.php';
-require_once 'maphper/relation/manymany.php';
-require_once 'maphper/datasource/database.php';
-require_once 'maphper/datasource/databaseadapter.php';
-require_once 'maphper/datasource/sqliteadapter.php';
-require_once 'maphper/datasource/mysqladapter.php';
-
+spl_autoload_register(function($class) {
+	$file = strtolower(str_replace('\\', DIRECTORY_SEPARATOR, $class)) . '.php';
+	if (is_file($file)) require_once $file;
+});
