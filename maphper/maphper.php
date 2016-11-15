fb5db4cdc010ba4f0d87dce25c105163b81ed943
Richard <solleer@hotmail.com>
2016-11-14 21:51:56 -0500

    Fix issue when calling `item` would cause an infinite loop

--- maphper/maphper.php
+++ maphper/maphper.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 namespace Maphper;
 class Maphper implements \Countable, \ArrayAccess, \Iterator {
 	const FIND_EXACT 		= 	0x1;
@@ -14,10 +14,10 @@ class Maphper implements \Countable, \ArrayAccess, \Iterator {
 	const FIND_NOT 			= 	0x400;
 	const FIND_BETWEEN		= 	0x800;
 	const FIND_NOCASE		= 	0x1000;
-	
+
 	private $dataSource;
-	private $relations = [];	
-	private $settings = ['filter' => [], 'sort' => null, 'limit' => null, 'offset' => null, 'resultClass' => '\\stdClass'];	
+	private $relations = [];
+	private $settings = ['filter' => [], 'sort' => null, 'limit' => null, 'offset' => null, 'resultClass' => '\\stdClass'];
 	private $array = [];
 	private $iterator = 0;
 
@@ -31,7 +31,7 @@ class Maphper implements \Countable, \ArrayAccess, \Iterator {
 			throw new \InvalidArgumentException('The mapper writeClosure must be an anonymous function');
 		}
 	}
-	
+
 	public function addRelation($name, Relation $relation) {
 		$this->relations[$name] = $relation;
 	}
@@ -56,47 +56,51 @@ class Maphper implements \Countable, \ArrayAccess, \Iterator {
 	public function current() {
 		return $this->wrap($this->createNew($this->array[$this->iterator]));
 	}
-		
+
 	public function key() {
 		$pk = $this->dataSource->getPrimaryKey();
 		$pk = end($pk);
 		return $this->array[$this->iterator]->$pk;
 	}
-	
+
 	public function next() {
 		++$this->iterator;
 	}
-	
+
 	public function valid() {
 		return isset($this->array[$this->iterator]);
-	}	
+	}
 
 	public function rewind() {
 		$this->iterator = 0;
+		$this->fillArrayValues();
+	}
 
+	public function item($n) {
+		$this->fillArrayValues();
+		return isset($this->array[$n]) ? $this->wrap($this->array[$n]) : null;
+	}
+
+	private function fillArrayValues() {
 		foreach ($this->settings['filter'] as $name => &$filter) {
-			
+
 			if (isset($this->relations[$name])) {
 				$this->relations[$name]->overwrite($filter, $filter[$name]);
 			}
 		}
-		if (empty($this->array)) $this->array = $this->dataSource->findByField($this->settings['filter'], ['order' => $this->settings['sort'], 'limit' => $this->settings['limit'], 'offset' => $this->settings['offset'] ]);
+		if (empty($this->array)) $this->array = $this->dataSource->findByField($this->settings['filter'],
+			['order' => $this->settings['sort'], 'limit' => $this->settings['limit'], 'offset' => $this->settings['offset'] ]);
 	}
-	
-	public function item($n) {
-		$this->rewind();
-		return isset($this->array[$n]) ? $this->wrap($this->array[$n]) : null;
-	}
-	
+
 	private function processFilters($value) {
 		//When saving to a mapper with filters, write the filters back into the object being stored
-		foreach ($this->settings['filter'] as $key => $filterValue) {			
+		foreach ($this->settings['filter'] as $key => $filterValue) {
 			if (empty($value->$key) && !is_array($filterValue)) $value->$key = $filterValue;
 		}
 		return $value;
 	}
 
-	public function offsetSet($offset, $value) {	
+	public function offsetSet($offset, $value) {
 		if ($value instanceof \Maphper\Relation) throw new \Exception();
 
 		$value = $this->processFilters($value);
@@ -105,17 +109,17 @@ class Maphper implements \Countable, \ArrayAccess, \Iterator {
 		$value = $this->wrap($value);
 		$this->dataSource->save($value);
 	}
-	
+
 	public function offsetExists($offset) {
 		return (bool) $this->dataSource->findById($offset);
 	}
-	
+
 	public function offsetUnset($id) {
 		$this->dataSource->deleteById($id);
 	}
 
 	public function offsetGet($offset) {
-		if (count($this->dataSource->getPrimaryKey()) > 1) return new MultiPk($this, $offset, $this->dataSource->getPrimaryKey());			
+		if (count($this->dataSource->getPrimaryKey()) > 1) return new MultiPk($this, $offset, $this->dataSource->getPrimaryKey());
 		return $this->wrap($this->createNew($this->dataSource->findById($offset)));
 	}
 
@@ -128,23 +132,23 @@ class Maphper implements \Countable, \ArrayAccess, \Iterator {
 				else $write($key, $this->dataSource->processDates($value));
 			}
 		}
-		return $obj;		
+		return $obj;
 	}
-	
+
 	private function wrap($object) {
 		//see if any relations need overwriting
 		foreach ($this->relations as $name => $relation) {
-			if (isset($object->$name) && !($object->$name instanceof \Maphper\Relation) ) {					
+			if (isset($object->$name) && !($object->$name instanceof \Maphper\Relation) ) {
 				//After overwriting the relation, does the parent object ($object) need overwriting as well?
 				if ($relation->overwrite($object, $object->$name)) $this[] = $object;
 
 			}
 
-			$object->$name = $relation->getData($object); 
-		}			
-		return $object;		
+			$object->$name = $relation->getData($object);
+		}
+		return $object;
 	}
-	
+
 	public function __call($method, $args) {
 		if (array_key_exists($method, $this->settings)) {
 			$maphper = new Maphper($this->dataSource, $this->settings, $this->relations);
@@ -154,11 +158,11 @@ class Maphper implements \Countable, \ArrayAccess, \Iterator {
 		}
 		else throw new \Exception('Method Maphper::' . $method . ' does not exist');
 	}
-	
+
 	public function findAggregate($function, $field, $group = null) {
 		return $this->dataSource->findAggregate($function, $field, $group, $this->settings['filter']);
 	}
-	
+
 	public function delete() {
 		$this->array = [];
 		$this->dataSource->deleteByField($this->settings['filter'], ['order' => $this->settings['sort'], 'limit' => $this->settings['limit'], 'offset' => $this->settings['offset']]);
