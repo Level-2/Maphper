aa7f147e58e242303a03704b7a42a52c10617052
Richard <solleer@hotmail.com>
2016-12-28 15:54:28 -0500

    Fix issue when using relation one

--- maphper/relation/one.php
+++ maphper/relation/one.php
@@ -7,48 +7,52 @@ class One implements \Maphper\Relation {
 	private $parentObject;
 	private $criteria = [];
 	private $data;
-	
+
 	public function __construct(\Maphper\Maphper $mapper, $parentField, $localField, array $criteria = []) {
 		$this->mapper = $mapper;
 		$this->parentField = $parentField;
 		$this->localField = $localField;
 		$this->criteria = $criteria;
 	}
-	
+
 	public function getData($parentObject) {
 		//Don't actually fetch the related data, return an instance of $this that will lazy load data when __get is called
 		$clone = clone $this;
 		$clone->parentObject = $parentObject;
-		return $clone;		
-	}	
-	
+		return $clone;
+	}
+
 	private function lazyLoad() {
 		if (!isset($this->data)) {
 
-			if ($this->parentObject == null) throw new \Exception('Error, no object set');			
+			if ($this->parentObject == null) throw new \Exception('Error, no object set');
 
 			if ($this->criteria) $this->data = $this->mapper->filter($this->criteria)->filter([$this->localField => $this->parentObject->{$this->parentField}])->item(0);
 			else $this->data = $this->mapper->filter([$this->localField => $this->parentObject->{$this->parentField}])->item(0);
 		}
 		return $this->data;
 	}
+	
 	public function __call($func, array $args = []) {
 		if ($this->lazyLoad() == null) return '';
 		return call_user_func_array([$this->lazyLoad(), $func], $args);
 	}
-	
+
 	public function __get($name) {
 		if ($this->lazyLoad()) return $this->lazyLoad()->$name;
 	}
-	
-	
+
+	public function __isset($name) {
+		return (bool) $this->__get($name);
+	}
+
 	public function overwrite($parentObject, &$data) {
 		$this->mapper[] = $data;
 		if (!isset($parentObject->{$this->parentField}) || $parentObject->{$this->parentField} != $data->{$this->localField}) {
 			$parentObject->{$this->parentField} = $data->{$this->localField};
 			//Trigger an update of the parent object
 			return true;
-		}		
+		}
 	}
 
 	public function getFilter($object) {
