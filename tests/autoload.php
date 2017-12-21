25ecf55fca7a9631b79f0617b81d5cad655511cc
Tom Butler <tom@r.je>
2017-12-21 14:07:14 +0000

    support for phpunit 6

--- tests/autoload.php
+++ tests/autoload.php
@@ -1,4 +1,9 @@
 <?php
+//Add support for PHPUnit 5 and 6
+if (!class_exists('PHPUnit_Framework_TestCase')) {
+	class PHPUnit_Framework_TestCase extends \PHPUnit\Framework\TestCase {}
+}
+
 require_once 'tests/testclasses.php';
 spl_autoload_register(function($class) {
 	$file = strtolower(str_replace('\\', DIRECTORY_SEPARATOR, $class)) . '.php';
